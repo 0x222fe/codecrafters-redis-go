b758b1dd18c6b9dda7549f15f98149d28cd88ab6
Jason Wong <42604000+0x222fe@users.noreply.github.com>
2025-07-13 00:32:32 +0800

    ACKs with no commands

--- internal/command/replconfHandler.go
+++ internal/command/replconfHandler.go
@@ -1,12 +1,43 @@
 package command
 
 import (
-	"github.com/0x222fe/codecrafters-redis-go/internal/resp"
+	"errors"
+	"strconv"
+	"strings"
+
 	"github.com/0x222fe/codecrafters-redis-go/internal/state"
+	"github.com/0x222fe/codecrafters-redis-go/internal/utils"
 )
 
 func replconfHandler(state *state.AppState, args []string) ([]byte, error) {
-	//INFO: ignore args for now
+	if len(args) == 0 {
+		return nil, errors.New("REPLCONF requires at least one argument")
+	}
 
-	return resp.NewRESPString("OK").Encode(), nil
+	subcommand := strings.ToUpper(args[0])
+
+	switch subcommand {
+	case "GETACK":
+		return replconfGetAck(state, args[1:])
+	// case "ACK":
+	// 	return replconfAck(state, args[1:])
+	default:
+		return nil, errors.New("Unknown REPLCONF subcommand: " + subcommand)
+	}
 }
+
+func replconfGetAck(_ *state.AppState, args []string) ([]byte, error) {
+	if len(args) != 1 {
+		return nil, errors.New("REPLCONF GETACK requires exactly one argument")
+	}
+
+	return utils.EncodeStringSliceToRESP([]string{"REPLCONF", "ACK", strconv.Itoa(0)}), nil
+}
+
+// func replconfAck(state *state.AppState, args []string) ([]byte, error) {
+// 	if len(args) != 1 {
+// 		return nil, errors.New("REPLCONF ACK requires exactly one argument")
+// 	}
+//
+// 	return replconfGetAck(state, args)
+// }
