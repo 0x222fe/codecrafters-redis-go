710309d0e5aef16460ff63608fc68e30e199e8de
Jason Wong <42604000+0x222fe@users.noreply.github.com>
2025-07-17 19:51:06 +0800

    query multiple streams using XREAD

--- internal/command/xreadHandler.go
+++ internal/command/xreadHandler.go
@@ -10,27 +10,38 @@ import (
 )
 
 func xreadHandler(req *request.Request, args []string) error {
-	if len(args) == 0 {
-		return errors.New("XREAD requires at least one argument")
+	if len(args) < 3 {
+		return errors.New("XREAD requires at least 3 arguments")
 	}
-	key, idStr := args[1], args[2]
 
-	id, err := store.ParseStreamEntryID(idStr)
-	if err != nil {
-		return err
+	count := len(args) - 1 // XREAD streams
+	if count%2 != 0 {
+		return errors.New("XREAD must have even numer of arguments after 'streams'")
 	}
-	id.Seq++
+	count /= 2
+	keys, idStrs := args[1:1+count], args[1+count:]
+	ids := make([]store.StreamEntryID, 0, count)
+	for i := 0; i < count; i++ {
+		id, err := store.ParseStreamEntryID(idStrs[i])
+		if err != nil {
+			return err
+		}
+		id.Seq++
+		ids = append(ids, id)
+	}
 
-	stream, ok := req.State.GetStore().GetStream(key)
 	arr := make([]resp.RESPValue, 0)
-	if ok {
-		entries := stream.Range(id.RadixKey(), nil)
-		streamArr := make([]resp.RESPValue, 0, 2*len(entries))
+	for i, key := range keys {
+		stream, ok := req.State.GetStore().GetStream(key)
+		if ok {
+			entries := stream.Range(ids[i].RadixKey(), nil)
+			streamArr := make([]resp.RESPValue, 0, 2*len(entries))
 
-		streamArr = append(streamArr, resp.NewRESPBulkString(&key))
-		streamEntryRESP := utils.StreamEntriesToRESPArray(entries)
-		streamArr = append(streamArr, streamEntryRESP)
-		arr = append(arr, resp.NewRESPArray(streamArr))
+			streamArr = append(streamArr, resp.NewRESPBulkString(&key))
+			streamEntryRESP := utils.StreamEntriesToRESPArray(entries)
+			streamArr = append(streamArr, streamEntryRESP)
+			arr = append(arr, resp.NewRESPArray(streamArr))
+		}
 	}
 	encoded := resp.NewRESPArray(arr).Encode()
 	writeResponse(req.Client, encoded)
