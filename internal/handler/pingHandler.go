a61669f85262e95be4e4cf9d91bf83ed6e196749
Jason Wong <42604000+0x222fe@users.noreply.github.com>
2025-07-31 15:18:51 +0800

    PING in sub mode

--- internal/handler/pingHandler.go
+++ internal/handler/pingHandler.go
@@ -3,11 +3,15 @@ package handler
 import (
 	"github.com/0x222fe/codecrafters-redis-go/internal/request"
 	"github.com/0x222fe/codecrafters-redis-go/internal/resp"
+	"github.com/0x222fe/codecrafters-redis-go/internal/utils"
 )
 
 func pingHandler(req *request.Request, args []string) error {
 	if req.Propagated {
 		return nil
+	}
+	if req.SubMode {
+		return writeResponse(req, utils.BulkStringsToRESPArray([]string{"pong", ""}))
 	}
 	return writeResponse(req, resp.NewRESPString("PONG"))
 }
