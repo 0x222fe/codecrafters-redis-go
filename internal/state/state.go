279c1e77858220c6a1bf37634a85e1f857428049
Jason Wong <42604000+0x222fe@users.noreply.github.com>
2025-07-31 14:22:30 +0800

    subscribe to a channel

--- internal/state/state.go
+++ internal/state/state.go
@@ -11,6 +11,10 @@ import (
 	"github.com/google/uuid"
 )
 
+const (
+	subChanBufSize = 16
+)
+
 type Replica struct {
 	Client     *client.Client
 	Offset     int
@@ -19,20 +23,37 @@ type Replica struct {
 	Cancel     context.CancelFunc
 }
 
+type Subscriber struct {
+	Client   *client.Client
+	Channels map[string]struct{}
+	MsgChan  chan PubSubMsg
+	Ctx      context.Context
+	Cancel   context.CancelFunc
+}
+
+type PubSubMsg struct {
+	Channel string
+	Payload []byte
+}
+
 type AppState struct {
-	mu       sync.RWMutex
-	cfg      *config.Config
-	store    *store.Store
-	replicas map[uuid.UUID]*Replica
-	state    *State
+	mu          sync.RWMutex
+	cfg         *config.Config
+	store       *store.Store
+	replicas    map[uuid.UUID]*Replica
+	subscribers map[uuid.UUID]*Subscriber
+	channelSubs map[string]map[uuid.UUID]*Subscriber
+	state       *State
 }
 
 func NewAppState(s *State, cfg *config.Config, store *store.Store) *AppState {
 	return &AppState{
-		cfg:      cfg,
-		store:    store,
-		state:    s,
-		replicas: make(map[uuid.UUID]*Replica),
+		cfg:         cfg,
+		store:       store,
+		state:       s,
+		replicas:    make(map[uuid.UUID]*Replica),
+		subscribers: make(map[uuid.UUID]*Subscriber),
+		channelSubs: make(map[string]map[uuid.UUID]*Subscriber),
 	}
 }
 
@@ -72,6 +93,67 @@ func (s *AppState) ReadCfg() config.Config {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 	return *s.cfg
+}
+
+func (s *AppState) AddSubscriber(c *client.Client, channel string) *Subscriber {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	sub, ok := s.subscribers[c.ID]
+	if !ok {
+		ctx, cancel := context.WithCancel(context.Background())
+		sub = &Subscriber{
+			Client:   c,
+			Ctx:      ctx,
+			Channels: make(map[string]struct{}),
+			Cancel:   cancel,
+			MsgChan:  make(chan PubSubMsg, subChanBufSize),
+		}
+		s.subscribers[c.ID] = sub
+	}
+	sub.Channels[channel] = struct{}{}
+
+	chanMap, ok := s.channelSubs[channel]
+	if !ok {
+		chanMap = make(map[uuid.UUID]*Subscriber)
+		s.channelSubs[channel] = chanMap
+	}
+
+	chanMap[c.ID] = sub
+
+	fmt.Printf("Subscriber connected: %s\n", c.RemoteAddr().String())
+
+	return sub
+}
+
+func (s *AppState) RemoveSubscriber(id uuid.UUID) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if sub, exists := s.subscribers[id]; exists {
+		sub.Cancel()
+
+		for channel := range sub.Channels {
+			delete(s.channelSubs[channel], id)
+		}
+
+		delete(s.subscribers, id)
+		fmt.Printf("Subscriber disconnected: %s\n", sub.Client.RemoteAddr().String())
+	}
+}
+
+func (s *AppState) Publish(channel string, payload []byte) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	chanMap, ok := s.channelSubs[channel]
+	if !ok {
+		return
+	}
+
+	for _, sub := range chanMap {
+		sub.Client.Write(payload)
+	}
 }
 
 func (s *AppState) AddReplica(c *client.Client) {
